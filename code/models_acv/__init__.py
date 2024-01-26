aef9af3332ac72cced5bc3c62afc5792dcca9a07
Akilax0 <killerilaksha@gmail.com>
2024-01-26 06:30:37 +0530

    loading teacher network

--- code/models_acv/__init__.py
+++ code/models_acv/__init__.py
@@ -1,6 +1,6 @@
-from models_acv.CGF_ACV import ACVNet
-from models_acv.loss import acv_model_loss_train, acv_model_loss_test, acv_model_loss_train_attn_only, acv_model_loss_train_freeze_attn
+from models_acv.acv import ACVNet
+from models_acv.loss import acv_model_loss_train_attn_only, acv_model_loss_train_freeze_attn, acv_model_loss_train, acv_model_loss_test
 
-__tmodels__ = {
+__t_models__ = {
     "acvnet": ACVNet
 }
